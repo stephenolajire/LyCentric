f750adadfdef45dc18402264bba30802d949e766
olajirestephen <horlharmighty2000@gmail.com>
2024-10-25 08:32:40 -0500

    remove Chatbot urls

--- backend/Project/urls.py
+++ backend/Project/urls.py
@@ -26,7 +26,6 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('Store.urls')),
-    path('api/', include('User.urls')),
     path('api/', include('ChatBox.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
