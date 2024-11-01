ee22a74c575fd79338150a327bc02b08faa37303
olajirestephen <horlharmighty2000@gmail.com>
2024-10-31 19:02:57 -0500

    changed verified page'

--- frontend/src/user/Verified.jsx
+++ frontend/src/user/Verified.jsx
@@ -1,16 +1,20 @@
 // Verified.js in React
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { MdVerified } from "react-icons/md";
+import style from "../css/Verified.module.css";
 
 const Verified = () => {
     return (
-        <div style={{padding:"4rem 2rem", width:"100%", display:"flex", alignItems:"center", flexDirection:"column"}}>
-            <h1 style={{fontSize:"3.5rem"}}> Your email has been successfully verified!</h1>
-            <p style={{fontSize:"2.5rem"}}>You can now log in to your account.</p>
-            <Link to="/login" style={{color:"blue", fontSize:"1.6rem", marginTop:"1rem"}}>
-                Login
-            </Link>
+      <div className={style.containers}>
+        <div className={style.conts}>
+          <MdVerified className={style.icon} />
         </div>
+        <Link to="/login">
+          <button className={style.homeBtn}>click to login</button>
+        </Link>
+        <h2>Email has been Verified</h2>
+      </div>
     );
 };
 
