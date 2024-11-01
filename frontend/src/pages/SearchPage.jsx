6aa83c44d50ab6dc7a98eb81fe7d4ea3c5e973e6
olajirestephen <horlharmighty2000@gmail.com>
2024-11-01 09:39:45 -0500

    added spinner to searchpage

--- frontend/src/pages/SearchPage.jsx
+++ frontend/src/pages/SearchPage.jsx
@@ -3,18 +3,22 @@ import { useSearchParams } from "react-router-dom";
 import styles from "../css/SearchPage.module.css";
 import api from "../constant/api";
 import ProductCard from "../components/Card";
+import Spinner from "../components/Spinner";
 
 const SearchPage = () => {
   const [searchParams] = useSearchParams();
-  const query = searchParams.get('q'); // Get the search term from the query parameter
+  const query = searchParams.get("q"); // Get the search term from the query parameter
   const [products, setProducts] = useState([]);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(true);
 
   const fetchSearchResults = async () => {
     try {
+      setLoading(true);
       const response = await api.get(`api/products/search/?q=${query}`);
-      setProducts(response.data.results); 
-      console.log(response.data.results)// Update state with search results
+      setProducts(response.data.results);
+      setLoading(false);
+      console.log(response.data.results); // Update state with search results
     } catch (err) {
       setError("Error fetching search results. Please try again.");
       console.error(err);
@@ -27,22 +31,22 @@ const SearchPage = () => {
     }
   }, [query]);
 
+  if (loading) {
+    return <Spinner />;
+  }
+
   return (
     <section className={styles.section}>
       <h3 className={styles.all}>Search Results for "{query}"</h3>
-      {error ? (
-        <p>{error}</p>
-      ) : (
-        <div className={styles.grid}>
-          {products.length > 0 ? (
-            products.map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))
-          ) : (
-            <p className={styles.query}>No products found for "{query}".</p>
-          )}
-        </div>
-      )}
+      <div className={styles.grid}>
+        {products.length > 0 ? (
+          products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))
+        ) : (
+          <p className={styles.query}>No products found for "{query}".</p>
+        )}
+      </div>
     </section>
   );
 };
